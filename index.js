a194f000a97ded02c2f2e79ebc0565395fa18fe3
alvincao <alvincao@tencent.com>
2022-07-31 21:48:59 +0800

    更新题目

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const example = [
-    [1, 5, 2]
+    3, [[0, 1, 100], [1, 2, 100], [0, 2, 500]],
+    0, 2, 1
     // covert2BinaryTree([2, 1, 3, null, 4])
 ]
 
 console.log('param', ...example)
 // const example = covertList([1,1,2]);
-console.log('Result: ', PredictTheWinner(...example))
+console.log('Result: ', findCheapestPrice(...example))
 
 /**
 let obj = null;
@@ -17,3 +18,7 @@ example[0].forEach((key, index) => {
     console.log(obj[key](...example[1][index]));
 })
 */
+
+// 'abcde'
+// 0,3 Math.abs(a-b)
+// 0+3  a+length-b  3 length
