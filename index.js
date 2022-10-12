262be32c250b943e6ca7c7f46843941da13f3511
alvincao <alvincao@tencent.com>
2022-10-12 14:38:07 +0800

    修改代码

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const example = [
-    covert2BinaryTree([5, 4, 8, 11, null, 13, 4, 7, 2, null, null, null, 1]),
-    22
+    '456',
+    '77'
     // covert2BinaryTree([2, 1, 3, null, 4, null, 7])
     // covertList([4, 2, 1, 3])
     // covert2BinaryTree([-10, 9, 20, null, null, 15, 7])
@@ -8,7 +8,7 @@ const example = [
 
 console.log('param', ...example)
 // const example = covertList([1,1,2]);
-const result = hasPathSum(...example)
+const result = addStrings(...example)
 
 console.log('Result: ', result)
 document.querySelector('#result').innerHTML = JSON.stringify(result, undefined, 4)
